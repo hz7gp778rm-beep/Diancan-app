37348d4f4daa9c052c9cd34b4fd7a1ce0246c61e
hz7gp778rm-beep <hz7gp778rm@privaterelay.appleid.com>
2026-06-17 14:32:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'diancan-v1';
+const CACHE_NAME = 'diancan-v2';
 const urlsToCache = [
   'index.html',
   'manifest.json',
@@ -9,6 +9,20 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
